dbdc1a5a93db7aeacf1a2ec5ac76f81ee7c98f0d
katty azabache <kattyazabachez@gmail.com>
2019-06-01 16:08:41 -0500

    card food

--- lib/presentation/food_create.dart
+++ lib/presentation/food_create.dart
@@ -55,6 +55,69 @@ class FoodCreate extends StatelessWidget {
               height: 32,
             ),
             ListHorizontalFood(),
+            Container(
+              constraints: BoxConstraints(minHeight: 120),
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black12.withOpacity(0.05),
+                    blurRadius: 9.0,
+                    offset: Offset(0.0, 5.0),
+                  ),
+                ],
+              ),
+              child: Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(height: 8),
+                        Text(
+                          'Titulo de la comida',
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'description',
+                          style: Theme.of(context).textTheme.body1,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 8),
+                          height: 2,
+                          width: 18,
+                          color: Colors.cyan,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.timer,
+                              size: 15,
+                            ),
+                            Container(width: 8.0),
+                            Text(
+                              '30` min',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
